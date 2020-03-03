5f766fc6309a17f5f5599e42c196dd42b4e5b444
picanum <10380748+picanum@users.noreply.github.com>
2020-03-03 20:56:18 +0100

    Update 12_02_2020.R

--- 12_02_2020.R
+++ 12_02_2020.R
@@ -103,3 +103,83 @@ korea[29:34,] %>%
         #panel.background = element_rect(fill = "#ffffcc"),
         #panel.grid = element_line(colour = "#919191", alpha =.5),
         plot.background = element_rect(fill = "#BFD5E3"))
+
+#### GRÁFICOS DE LA TASA DE LETALIDAD (CASE FATALITY RATE) ####
+
+#Creamos las bases de datos con los casos confirmados y fallecimientos de la misma forma que antes
+conf2 <- reshape2::melt(confirmados, id.vars = colnames(confirmados)[1:4])
+conf2$Fechas <- as.POSIXct(paste("0",as.character(conf2$variable),sep=""), format = "%m/%e/%y")
+
+dat_graf <- conf2 %>%
+  filter(`Province/State` == "Hubei") %>%
+  mutate(cifra_dif = c(NA, diff(value))) %>%
+  filter(cifra_dif>50) %>%
+  mutate(cifra_dif = c(NA, diff(value)),fechas_dif = c(NA, diff(Fechas)))
+
+fall2 <- reshape2::melt(muertes, id.vars = colnames(muertes)[1:4])
+fall2$Fechas <- as.POSIXct(paste("0",as.character(fall2$variable),sep=""), format = "%m/%e/%y")
+
+fall_graf <- fall2 %>%
+  filter(`Province/State` == "Hubei") %>%
+  mutate(cifra_dif = c(NA, diff(value))) %>%
+  filter(cifra_dif>0) %>%
+  mutate(cifra_dif = c(NA, diff(value)),fechas_dif = c(NA, diff(Fechas)))
+
+recu2 <- reshape2::melt(recuperados, id.vars = colnames(recuperados)[1:4])
+recu2$Fechas <- as.POSIXct(paste("0",as.character(recu2$variable),sep=""), format = "%m/%e/%y")
+
+#Creamos ahora un dataset para las recuperaciones de manera analoga, pero al final le metemos un right_join para que nos junte con el dataset de casos
+recu_graf <- recu2 %>%
+  filter(`Province/State` == "Hubei") %>%
+  mutate(cifra_dif = c(NA, diff(value))) %>%
+  filter(cifra_dif>0) %>%
+  mutate(cifra_dif = c(NA, diff(value)),fechas_dif = c(NA, diff(Fechas))) %>%
+  right_join(dat_graf, by = "variable")
+
+#Y despues juntamos tambien el de fallecimientos con un left_join
+recu_graf <- recu_graf %>% left_join(fall_graf, by = "variable")
+
+#Dado que los reportes de casos y fallecimientos llegaban desacompasados, conviene hacer unos arreglos antes de graficar
+recu_graf[28,"cifra_dif.x"] <- recu_graf[27,"cifra_dif.x"] + recu_graf[28,"cifra_dif.x"]
+recu_graf[27,"cifra_dif.x"] <- NA
+recu_graf[27,"fechas_dif.x"] <- NA
+recu_graf[28,"fechas_dif.x"] <- 2
+
+recu_graf[28,"cifra_dif.y"] <- recu_graf[27,"cifra_dif.y"] + recu_graf[28,"cifra_dif.y"]
+recu_graf[27,"cifra_dif.y"] <- NA
+recu_graf[27,"fechas_dif.y"] <- NA
+recu_graf[28,"fechas_dif.y"] <- 2
+
+recu_graf[19,"cifra_dif.x"] <- recu_graf[19,"cifra_dif.x"] + 47
+recu_graf[19,"fechas_dif.x"] <- 2
+recu_graf[6,"cifra_dif.x"] <- recu_graf[6,"cifra_dif.x"] + 8
+recu_graf[6,"fechas_dif.x"] <- 2
+
+#El codigo de los graficos es analogo practicamente a los anteriores tambien
+#Solo hay que tener en cuenta lo que significa cada variable
+#     - value = nº acumulado de fallecidos hasta un dia concreto
+#     - value.y = nº acumulado de casos confirmados hasta un dia concreto
+#     - value.x = nº acumulado de recuperaciones hasta un dia concreto
+recu_graf %>% ggplot(aes(x = Fechas, y = value*100/value.y)) + 
+  geom_point(size = 3) + geom_line(size = 1.05) +
+  stat_smooth(se = F, size = 1.1) +
+  scale_x_datetime(date_breaks = "2 days", date_labels = "%d/%m") +
+  labs(title = "Evolución de la tasa de letalidad bruta (CFR) del COVID-19\n(nº fallecimientos/nº casos) en la región de Hubei (China) a 03-03-2020",
+       subtitle = "Fuente: Centro para Ciencia de Sistemas e Ingeniería (CSSE) de la Universidad Johns Hopkins\n#DatosDeMiercoles de @R4DS_es, semana 12-02-2020",
+       x = "Fecha", y = "Tasa de letalidad bruta (%)",
+       caption = "@Picanumeros") +
+  theme_bw(base_size = 16) +
+  theme(text = element_text(family = "Liberation Sans"),
+        plot.background = element_rect(fill = "#BFD5E3"))
+
+recu_graf %>% ggplot(aes(x = Fechas, y = value*100/(value + value.x))) + 
+  geom_point(size = 3) + geom_line(size = 1.05) +
+  stat_smooth(se = F, size = 1.1) +
+  scale_x_datetime(date_breaks = "2 days", date_labels = "%d/%m") +
+  labs(title = "Evolución de la tasa de letalidad bruta (CFR) del COVID-19\n[nº fallecimientos/(nº fallecimientos + nº recuperaciones)] en la región de Hubei (China) a 03-03-2020",
+       subtitle = "Fuente: Centro para Ciencia de Sistemas e Ingeniería (CSSE) de la Universidad Johns Hopkins\n#DatosDeMiercoles de @R4DS_es, semana 12-02-2020",
+       x = "Fecha", y = "Tasa de letalidad bruta (%)",
+       caption = "@Picanumeros") +
+  theme_bw(base_size = 16) +
+  theme(text = element_text(family = "Liberation Sans"),
+        plot.background = element_rect(fill = "#BFD5E3"))
